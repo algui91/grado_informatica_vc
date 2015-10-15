d3db415b0af084545c4cf77dbecc1213724fb2c2
Alejandro Alcalde <algui91@gmail.com>
2015-10-15 23:16:48 +0200

    Handling empty parameters

--- P1/Utils.cpp
+++ P1/Utils.cpp
@@ -35,35 +35,39 @@ Mat myGetGaussianKernel1D(double sigma) {
 
 Mat convolutionOperator1D(Mat &signalVector, Mat &kernel, BorderTypes border) {
 
-    int extraBorder = kernel.cols / 2;
-
-    vector<Mat> signalVectorByChannels(signalVector.channels());
-    split(signalVector, signalVectorByChannels);
-
     Mat filtered;
 
-    for (vector<Mat>::const_iterator it = signalVectorByChannels.begin(); it != signalVectorByChannels.end(); ++it) {
-        Mat m = *(it);
-        // Create a new Mat with the extra borders needed
-        Mat signalWithBorder(1, m.cols + kernel.cols / 2, m.type());
-        // Add extra borders to the vector to solve boundary issue
-        copyMakeBorder(m, signalWithBorder, 0, 0, extraBorder, extraBorder, border, Scalar(0));
-        // Vector to store the convolution result
-        filtered = m.clone();
-        // Create a ROI to pass along the vector and compute convolution with the kernel
-        Mat roi(signalWithBorder, Rect(0, 0, kernel.cols, 1));
-        for (int i = 0; i < m.cols; i++) {
-            // Multiply the focused section by the kernel
-            Mat r = roi.mul(kernel);
-            // Sum the result of the above operation to the pixel at i
-            filtered.at<double>(i) = (double) *(sum(r).val);
-            // Move the Roi one position to the right
-            roi = roi.adjustROI(0, 0, -1, 1);
+    if (!signalVector.empty() || !kernel.empty()) {
+        int extraBorder = kernel.cols / 2;
+
+        vector<Mat> signalVectorByChannels(signalVector.channels());
+        split(signalVector, signalVectorByChannels);
+
+
+        for (vector<Mat>::const_iterator it = signalVectorByChannels.begin(); it != signalVectorByChannels.end(); ++it) {
+            Mat m = *(it);
+            // Create a new Mat with the extra borders needed
+            Mat signalWithBorder(1, m.cols + kernel.cols / 2, m.type());
+            // Add extra borders to the vector to solve boundary issue
+            copyMakeBorder(m, signalWithBorder, 0, 0, extraBorder, extraBorder, border, Scalar(0));
+            // Vector to store the convolution result
+            filtered = m.clone();
+            // Create a ROI to pass along the vector and compute convolution with the kernel
+            Mat roi(signalWithBorder, Rect(0, 0, kernel.cols, 1));
+            for (int i = 0; i < m.cols; i++) {
+                // Multiply the focused section by the kernel
+                Mat r = roi.mul(kernel);
+                // Sum the result of the above operation to the pixel at i
+                filtered.at<double>(i) = (double) *(sum(r).val);
+                // Move the Roi one position to the right
+                roi = roi.adjustROI(0, 0, -1, 1);
+            }
+            filtered.copyTo(m);
         }
-        filtered.copyTo(m);
+        // Merge the vectors into a multichannel Mat
+        merge(signalVectorByChannels, filtered);
     }
-    // Merge the vectors into a multichannel Mat
-    merge(signalVectorByChannels, filtered);
+
     return filtered;
 }
 
@@ -72,26 +76,28 @@ Mat convolutionOperator1D(Mat &signalVector, Mat &kernel, BorderTypes border) {
 Mat computeConvolution(Mat &m, double sigma) {
 
     // TODO only allow C1 or C3  CV_Assert(src.channels() == 3);
-    
-    Mat result = m.clone();
-    // Store type to restore it when the convolution is computed
-    int type = result.channels() == 1 ? CV_64F : CV_64FC3;
-    // Convert the image to a 64F type, (one or three channels)
-    result.convertTo(result, type);
-    Mat kernel = myGetGaussianKernel1D(sigma);
-    // This kernel is separable, apply convolution for rows and columns separately
-    for (int i = 0; i < result.rows; i++) {
-        Mat row = result.row(i);
-        row = convolutionOperator1D(row, kernel, BORDER_CONSTANT);
-        row.copyTo(result.row(i));
+    Mat result;
+
+    if (!m.empty()) {
+        result = m.clone();
+        // Store type to restore it when the convolution is computed
+        int type = result.channels() == 1 ? CV_64F : CV_64FC3;
+        // Convert the image to a 64F type, (one or three channels)
+        result.convertTo(result, type);
+        Mat kernel = myGetGaussianKernel1D(sigma);
+        // This kernel is separable, apply convolution for rows and columns separately
+        for (int i = 0; i < result.rows; i++) {
+            Mat row = result.row(i);
+            row = convolutionOperator1D(row, kernel, BORDER_CONSTANT);
+            row.copyTo(result.row(i));
+        }
+        for (int i = 0; i < result.cols; i++) {
+            Mat col = result.col(i);
+            col = convolutionOperator1D(col, kernel, BORDER_CONSTANT);
+            col.copyTo(result.col(i));
+        }
+        result.convertTo(result, m.type());
     }
-    for (int i = 0; i < result.cols; i++) {
-        Mat col = result.col(i);
-        col = convolutionOperator1D(col, kernel, BORDER_CONSTANT);
-        col.copyTo(result.col(i));
-    }
-    result.convertTo(result, m.type());
-    
     return result;
 }
 
