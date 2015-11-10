ce42a67a5bcf3a669a071f94eabf1071abb0ba5c
Alejandro Alcalde <algui91@gmail.com>
2015-11-10 23:29:34 +0100

    P2: ex2, using BRISK and ORB detectors

--- P2/main.cpp
+++ P2/main.cpp
@@ -54,10 +54,10 @@ int main() {
     Mat img3;
     warpPerspective(img1, img3, H, img1.size());
     
-    imshow("Original", img1);
-    imshow("Projection", img2);
-    imshow("WarpPerspectivr", img3);
-    waitKey(0);
+//    imshow("Original", img1);
+//    imshow("Projection", img2);
+//    imshow("WarpPerspectivr", img3);
+//    waitKey(0);
 
     // Pick points again, this time very close together
     p1(0) = 139;   p1(1) = 344;     p1(2) = 1;
@@ -92,12 +92,183 @@ int main() {
     H = T.inv() * Hn * T2;
     
     warpPerspective(img1, img3, H, img1.size());
-
-    imshow("Original", img1);
-    imshow("Projection", img2);
-    imshow("WarpPerspectivr", img3);
-    waitKey(0);
+//
+//    imshow("Original", img1);
+//    imshow("Projection", img2);
+//    imshow("WarpPerspectivr", img3);
+//    waitKey(0);
     
+   
+    // EXERCISE 2
+
+    img1 = imread("./imagenes/Yosemite1.jpg", IMREAD_GRAYSCALE);
+    img2 = imread("./imagenes/Yosemite2.jpg", IMREAD_GRAYSCALE);
+    
+    Ptr<Feature2D> detector = BRISK::create();
+    vector<DMatch> matches;
+    vector<KeyPoint> keypoints1, keypoints2;
+
+    detector->detect(img1, keypoints1);
+    Mat descriptors1, descriptors2;
+    detector->compute(img1, keypoints1, descriptors1);
+    detector->detectAndCompute(img2, Mat(), keypoints2, descriptors2, false);
+
+    Ptr<DescriptorMatcher> descriptorMatcher = DescriptorMatcher::create("BruteForce-Hamming");
+    
+    descriptorMatcher->match(descriptors1, descriptors2, matches, Mat());
+    
+    // Keep best matches only to have a nice drawing.
+    // We sort distance between descriptor matches
+    Mat index;
+    int nbMatch = int(matches.size());
+    Mat tab(nbMatch, 1, CV_32F);
+    for (int i = 0; i < nbMatch; i++) {
+        DMatch dm = matches[i];
+        tab.at<float>(i, 0) = dm.distance;
+    }
+    sortIdx(tab, index, SORT_EVERY_COLUMN + SORT_ASCENDING);
+    vector<DMatch> bestMatches;
+    for (int i = 0; i < 30; i++) {
+        bestMatches.push_back(matches[index.at<int>(i, 0)]);
+    }
+    
+    Mat result;
+    drawMatches(img1, keypoints1, img2, keypoints2, bestMatches, result);
+    namedWindow("BRISK:FlannBased", WINDOW_AUTOSIZE);
+    imshow("BRISK:FlannBased", result);
+    waitKey();
+    
+//    vector<String> typeDesc;
+//    vector<String> typeAlgoMatch;
+//    vector<String> fileName;
+//    // This descriptor are going to be detect and compute
+//    typeDesc.push_back("ORB"); // see http://docs.opencv.org/trunk/de/dbf/classcv_1_1BRISK.html
+//    typeDesc.push_back("BRISK"); // see http://docs.opencv.org/trunk/db/d95/classcv_1_1ORB.html
+//
+//    // This algorithm would be used to match descriptors see http://docs.opencv.org/trunk/db/d39/classcv_1_1DescriptorMatcher.html#ab5dc5036569ecc8d47565007fa518257
+//    typeAlgoMatch.push_back("BruteForce");
+//    typeAlgoMatch.push_back("BruteForce-L1");
+//    typeAlgoMatch.push_back("BruteForce-Hamming");
+//    typeAlgoMatch.push_back("BruteForce-Hamming(2)");
+//    fileName.push_back("./imagenes/Yosemite1.jpg");
+//    fileName.push_back("./imagenes/Yosemite2.jpg");
+//
+//    img1 = imread(fileName[0], IMREAD_GRAYSCALE);
+//    img2 = imread(fileName[1], IMREAD_GRAYSCALE);
+//
+//    if (img1.rows * img1.cols <= 0) {
+//        cout << "Image " << fileName[0] << " is empty or cannot be found\n";
+//        return (0);
+//    }
+//    if (img2.rows * img2.cols <= 0) {
+//        cout << "Image " << fileName[1] << " is empty or cannot be found\n";
+//        return (0);
+//    }
+//
+//    vector<double> desMethCmp;
+//    Ptr<Feature2D> b;
+//
+//    // Descriptor loop
+//    vector<String>::iterator itDesc;
+//    for (itDesc = typeDesc.begin(); itDesc != typeDesc.end(); itDesc++) {
+//        Ptr<DescriptorMatcher> descriptorMatcher;
+//        // Match between img1 and img2
+//        vector<DMatch> matches;
+//        // keypoint  for img1 and img2
+//        vector<KeyPoint> keyImg1, keyImg2;
+//        // Descriptor for img1 and img2
+//        Mat descImg1, descImg2;
+//        vector<String>::iterator itMatcher = typeAlgoMatch.end();
+//        if (*itDesc == "ORB") {
+//            b = ORB::create();
+//        } else if (*itDesc == "BRISK") {
+//            b = BRISK::create();
+//        }
+//        try {
+//            // We can detect keypoint with detect method
+//            b->detect(img1, keyImg1, Mat());
+//            // and compute their descriptors with method  compute
+//            b->compute(img1, keyImg1, descImg1);
+//            // or detect and compute descriptors in one step
+//            b->detectAndCompute(img2, Mat(), keyImg2, descImg2, false);
+//            // Match method loop
+//            for (itMatcher = typeAlgoMatch.begin(); itMatcher != typeAlgoMatch.end(); itMatcher++) {
+//                descriptorMatcher = DescriptorMatcher::create(*itMatcher);
+//                if ((*itMatcher == "BruteForce-Hamming" || *itMatcher == "BruteForce-Hamming(2)") && (b->descriptorType() == CV_32F || b->defaultNorm() <= NORM_L2SQR)) {
+//                    cout << "**************************************************************************\n";
+//                    cout << "It's strange. You should use Hamming distance only for a binary descriptor\n";
+//                    cout << "**************************************************************************\n";
+//                }
+//                if ((*itMatcher == "BruteForce" || *itMatcher == "BruteForce-L1") && (b->defaultNorm() >= NORM_HAMMING)) {
+//                    cout << "**************************************************************************\n";
+//                    cout << "It's strange. You shouldn't use L1 or L2 distance for a binary descriptor\n";
+//                    cout << "**************************************************************************\n";
+//                }
+//                try {
+//                    descriptorMatcher->match(descImg1, descImg2, matches, Mat());
+//                    // Keep best matches only to have a nice drawing.
+//                    // We sort distance between descriptor matches
+//                    Mat index;
+//                    int nbMatch = int(matches.size());
+//                    Mat tab(nbMatch, 1, CV_32F);
+//                    for (int i = 0; i < nbMatch; i++) {
+//                        DMatch dm = matches[i];
+//                        tab.at<float>(i, 0) = dm.distance;
+//                    }
+//                    sortIdx(tab, index, SORT_EVERY_COLUMN + SORT_ASCENDING);
+//                    vector<DMatch> bestMatches;
+//                    for (int i = 0; i < 30; i++) {
+//                        bestMatches.push_back(matches[index.at<int>(i, 0)]);
+//                    }
+//                    Mat result;
+//                    drawMatches(img1, keyImg1, img2, keyImg2, bestMatches, result);
+//                    namedWindow(*itDesc + ": " + *itMatcher, WINDOW_AUTOSIZE);
+//                    imshow(*itDesc + ": " + *itMatcher, result);
+//
+//                    vector<DMatch>::iterator it;
+//                    cout << "**********Match results**********\n";
+//                    cout << "Index \tIndex \tdistance\n";
+//                    cout << "in img1\tin img2\n";
+//                    // Use to compute distance between keyPoint matches and to evaluate match algorithm
+//                    double cumSumDist2 = 0;
+//                    for (it = bestMatches.begin(); it != bestMatches.end(); it++) {
+//                        DMatch dm = *(it);
+//                        cout << dm.queryIdx << "\t" << dm.trainIdx << "\t" << dm.distance << "\n";
+//                        KeyPoint s = keyImg1[dm.queryIdx];
+//                        KeyPoint s2 = keyImg2[dm.trainIdx];
+//                        Point2d p = s.pt - s2.pt;
+//                        cumSumDist2 = p.x * p.x + p.y * p.y;
+//                    }
+//                    desMethCmp.push_back(cumSumDist2);
+//                    waitKey();
+//                } catch (Exception& e) {
+//                    cout << e.msg << endl;
+//                    cout << "Cumulative distance cannot be computed." << endl;
+//                    desMethCmp.push_back(-1);
+//                }
+//            }
+//        } catch (Exception& e) {
+//            cout << "Feature : " << *itDesc << "\n";
+//            if (itMatcher != typeAlgoMatch.end()) {
+//                cout << "Matcher : " << *itMatcher << "\n";
+//            }
+//            cout << e.msg << endl;
+//        }
+//    }
+//    int i = 0;
+//    cout << "Cumulative distance between keypoint match for different algorithm and feature detector \n\t";
+//    cout << "We cannot say which is the best but we can say results are differents! \n\t";
+//    for (vector<String>::iterator itMatcher = typeAlgoMatch.begin(); itMatcher != typeAlgoMatch.end(); itMatcher++) {
+//        cout << *itMatcher << "\t";
+//    }
+//    cout << "\n";
+//    for (itDesc = typeDesc.begin(); itDesc != typeDesc.end(); itDesc++) {
+//        cout << *itDesc << "\t";
+//        for (vector<String>::iterator itMatcher = typeAlgoMatch.begin(); itMatcher != typeAlgoMatch.end(); itMatcher++, i++) {
+//            cout << desMethCmp[i] << "\t";
+//        }
+//        cout << "\n";
+//    }
     
     // If using findhomography, denormalize points
 //        Mat H2 = findHomography(p1, p2, RANSAC);
